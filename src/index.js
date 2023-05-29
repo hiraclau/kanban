8506c0ec739b29ddd2c53ce13d9ba60e21c54f67
Claudio Hirakawa <claudio.hirakawa@inteegra.com.br>
2023-05-29 10:52:23 -0300

    chore: pwa

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-serviceWorkerRegistration.register();
-
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(<App />);
+
+serviceWorkerRegistration.register();
